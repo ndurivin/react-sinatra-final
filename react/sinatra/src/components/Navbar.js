f508b7e7a1917da4077edf82dd837e930e950dea
Vinn Nduri <ndurivo@gmail.com>
2022-09-06 19:53:10 +0300

    Added the Hero component

--- react/sinatra/src/components/Navbar.js
+++ react/sinatra/src/components/Navbar.js
@@ -5,13 +5,13 @@ function Navbar() {
   return (
     <div>
         <nav className="navbar navbar-expand-lg bg-info">
-            <div className="container-fluid">
+            <div className="container-fluid container">
                 <Link to="/" className="navbar-brand">DevsLab</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
                     
                     </ul>
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
