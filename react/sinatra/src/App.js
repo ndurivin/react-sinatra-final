daa72b5d7bf0cc178caeeb55c3c9c1042950c923
Vinn Nduri <ndurivo@gmail.com>
2022-09-06 19:46:32 +0300

    Added the navbar

--- react/sinatra/src/App.js
+++ react/sinatra/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react'
+// import ReactDOM from 'react-dom/client';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { Home } from "./components/Home";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div>
+          <Navbar />
+        </div>
+        <Routes>
+          {/* <Route exact path="/" element={<Home />}></Route> */}
+          {/* <Route exact path="/devs" element={<Devs />}></Route> */}
+          {/* <Route exact path="/admin" element={<Admin />}></Route>
+          <Route exact path="/devs/:devId" element={<Profile />}></Route> */}
+        </Routes>
+      </Router>
   );
 }
 
--- react/sinatra/src/components/Navbar.js
+++ react/sinatra/src/components/Navbar.js
@@ -1,9 +1,44 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
     <div>
-      
+        <nav className="navbar navbar-expand-lg bg-info">
+            <div className="container-fluid">
+                <Link to="/" className="navbar-brand">DevsLab</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    
+                    </ul>
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                        <Link to="/" className="nav-link" aria-current="page">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/" className="nav-link" aria-current="page">About</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/" className="nav-link" aria-current="page">Devs</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/devs" className="nav-link" aria-current>Contact</Link>
+                    </li>
+
+                    <li className="nav-item">
+                        <Link to="/admin" className="nav-link active" aria-current>Admin</Link>
+                    </li>
+                    </ul>
+            </div>
+                    <form class="d-flex" role="search">
+                        {/* <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/> */}
+                        <button class="btn btn-outline-primary" type="submit">Login</button>
+                    </form>
+        </div>
+        </nav>
     </div>
   )
 }
