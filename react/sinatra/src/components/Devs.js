3d950b8fbe67f08bb806d7defbea2d363978cb45
Vinn Nduri <ndurivo@gmail.com>
2022-09-09 16:21:02 +0300

    Created individual profile component

--- react/sinatra/src/components/Devs.js
+++ react/sinatra/src/components/Devs.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from 'react-router-dom'
+import Footer from '../components/Footer'
 
 
 
@@ -26,11 +27,9 @@ import { Link } from 'react-router-dom'
 function Devs() {
   return (
     <div>
-      <h2>View Developers Here</h2>
       <div className="navbar navbar-default text-center" role="navigation">
         <h2 className="navbar-text col-md-12 col-sm-12 col-xs-12 text p-3">
-          This is the Developers Page
-        </h2>
+        View Developers Here</h2>
       </div>
       <div className="container">
       <div className="row row-cols-1 row-cols-md-4 g-4">
@@ -41,15 +40,40 @@ function Devs() {
               <h5 className="card-title">Developer Name</h5>
               <p className="card-text">Languages</p>
               <p className="card-text">Experience</p>
-              <p className="card-text">Developer Category</p>
+              <p className="card-text">Category</p>
+              <p className="card-text">Rating</p>
             <Link to="/about" className="btn btn-primary">View Developer</Link>
+            </div>
+          </div>
+        </div>
+      </div> <br/>
+      <button type="button" className="btn btn-primary btn-sm">Small button</button>
+      <button type="button" className="btn btn-secondary btn-sm">Small button</button>
+      <button type="button" className="btn btn-secondary btn-sm">Small button</button>
 
+      <h3 className="navbar-text col-md-12 col-sm-12 col-xs-12 text p-3">
+        Fullstack Developers</h3>
+
+        <div className="row row-cols-1 row-cols-md-4 g-4">
+        <div className="col">
+          <div className="card h-100 shadow ">
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Developer Name</h5>
+              <p className="card-text">Languages</p>
+              <p className="card-text">Experience</p>
+              <p className="card-text">Category</p>
+              <p className="card-text">Rating</p>
+            <Link to="/about" className="btn btn-primary">View Developer</Link>
             </div>
           </div>
         </div>
       </div>
+
       </div>
+      <Footer />
       </div>
+
   );
 }
 
--- react/sinatra/src/components/Login.js
+++ react/sinatra/src/components/Login.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React from "react";
 
 function Login() {
   return (
-    <div>
+    <div className="container">
       <div className="navbar navbar-default text-center" role="navigation">
-            <h2 className="navbar-text col-md-12 col-sm-12 col-xs-12 text p-3">This is the Login Page</h2>
+        <h3 className="navbar-text col-md-12 col-sm-12 col-xs-12 text p-3">
+          Login Here</h3>
+      </div>
+      <form>
+        <div className="mb-4">
+          <label for="exampleInputEmail1" className="form-label">Email address</label>
+          <input style={{width: 200}} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+          {/* <div id="emailHelp" className="form-text">Your email is safe with us.</div> */}
         </div>
+        <div className="mb-3">
+          <label for="exampleInputPassword1" className="form-label">Password</label>
+          <input type="password" className="form-control" id="exampleInputPassword1"/>
+        </div>
+        {/* <div className="mb-3 form-check">
+          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+          <label className="form-check-label" for="exampleCheck1">Check me out</label>
+        </div> */}
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     </div>
-  )
+  );
 }
 
 export default Login;
