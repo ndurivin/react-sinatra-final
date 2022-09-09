2eddf26bba088fe03ce247a2ec8fa875d123525d
Vinn Nduri <ndurivo@gmail.com>
2022-09-09 04:47:22 +0300

    Add the update code to react Admin component

--- react/sinatra/src/components/Admin.js
+++ react/sinatra/src/components/Admin.js
@@ -49,6 +49,32 @@ import React from 'react'
 //   return <form onSubmit={handleSubmit}>{/* controlled form code here*/}</form>;
 // }
 
+// HANDLING OF UPDATE REQUEST
+
+// function EditReviewForm({ review, onUpdateReview }) {
+//   const [comment, setComment] = useState("");
+//   const [score, setScore] = useState("0");
+
+//   function handleSubmit(e) {
+//     e.preventDefault();
+//     fetch(`http://localhost:9292/reviews/${review.id}`, {
+//       method: "PATCH",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         comment: comment,
+//         score: score,
+//       }),
+//     })
+//       .then((r) => r.json())
+//       .then((updatedReview) => onUpdateReview(updatedReview));
+//   }
+
+//   return <form onSubmit={handleSubmit}>{/* controlled form code here*/}</form>;
+// }
+
+
 
 //HANDLING OF DELETE REQUEST
 // function ReviewItem({ review, onDeleteReview }) {
