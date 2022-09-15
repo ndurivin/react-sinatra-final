02d7861c7f5c7b73eb0e1814f16ac40b518032aa
Vinn Nduri <ndurivo@gmail.com>
2022-09-15 06:04:01 +0300

    Fixed the post to db function using axios

--- react/sinatra/src/components/PostReview.js
+++ react/sinatra/src/components/PostReview.js
@@ -1,5 +1,7 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 // import { useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 
 
@@ -7,40 +9,42 @@ function PostReview() {
 
     const url = "http://localhost:9292/reviews"
 
+    const {state} = useLocation()
+    const {dev} = state
+    console.log(dev)
+
    
     const [inputs, setInputs] = useState({});
     const [data, setData] = useState([])
 
-// Get all data function
-const getData = () => {
-  fetch(url)
-  .then((res)=> res.json())
-  .then((json) => {
-    console.log(json);
-    setData(json);
-  })
-};
+    const [review_title, setReview_Title] = useState('')
+    const [review_desc, setReview_Desc] = useState('')
+    // const [developer_id, setDeveloper_Id] = useState('')
+    const [rating, setRating] = useState('')
+
+// Get all data from api function using Axios
 
 useEffect(()=> {
-  getData();
+  axios.get(url)
+  .then(res => {
+    console.log(res.data)
+    setData(res.data)
+  })
+  .catch(err => console.log(err))
 
 }, [])
 
-// Post data function
-const postData = () => {
-  fetch(url, {
-    method: "POST",
-    body: JSON.stringify({
-      review_title: inputs.title,
-      review_desc: inputs.desc,
-      rating: parseInt(inputs.rating)
-    }),
-    headers: {
-      "Content-Type": "applicattion/json; characterset-UTF-8",
-    },
-  })
-  .then((res)=> res.json())
-  .then((json)=> console.log(json));
+// Post review to api function
+
+const postData = (e) => {
+  e.preventDefault();
+  axios.post(url, {
+    review_title,
+    review_desc, 
+    rating
+  }).then(res => console.log('Posting Review', res))
+    .catch(err => console.log(err))
+
 };
 
 //Update data function using PUT
@@ -92,28 +96,34 @@ const deleteData = () => {
   return (
     <div>
       <form>
-      <div className="mb-3">
-        <input type="text" onChange={handleChange} name="review_title" placeholder='Review Title'></input>
+      <div className="mb-2">
+        <input type="text" value={review_title} onChange={(e) => setReview_Title(e.target.value)} name="review_title" placeholder='Review Title'></input>
         </div>
-        <div className="mb-3">
-        <input type="text" onChange={handleChange}  name="review_desc" placeholder='Review Description' row={2}></input>
+        <div className="mb-2">
+        <input type="text" value={review_desc}  onChange={(e) => setReview_Desc(e.target.value)}  name="review_desc" placeholder='Review Description' row={2}></input>
         </div>
-        <div className="mb-3">
-        <input type="number" onChange={handleChange}  name="rating" placeholder='Rating' min={1} max={10}></input>
+        {/* <div className="mb-2">
+        <input type="number" value={developer_id}  onChange={(e) => setReview_Title(e.target.value)}  name="developer_id" placeholder='Developer Id'></input>
+        </div> */}
+        <div className="mb-2">
+        <input type="number" value={rating}  onChange={(e) => setRating(e.target.value)}  name="rating" placeholder='Rating' min={1} max={10}></input>
         </div>
-        <button onClick={handleSubmit} type="button" className="btn btn-primary btn-sm" style={{margin: 5}} >Add</button>
+        <button onClick={postData} type="button" className="btn btn-primary btn-sm" style={{margin: 5}} >Add</button>
         <button onClick={updateData} type="button" className="btn btn-success btn-sm" style={{margin: 10}}>Update</button>
         <button onClick={deleteData} type="button" className="btn btn-danger btn-sm">Delete</button>
       </form>
-      <div>
+
+      <div className="p-5">
+        <h4 style={{color: "green"}}>Available Reviews</h4>
         <div className="row-cols-md-4 py-2">
-      {data.map((dev)=> (  
-        <div key={dev.id} className="col mb-4">
+      {data.map((rev)=> (  
+        <div key={rev.id} className="col mb-4">
           <div className="card text-center h-100 shadow">
             <div className="card-body ">
-              <h5 className="card-title">{data.title}</h5>
-              <p className="card-text">{data.desc}</p>
-              <p className="card-text">Rating: {data.rating}</p>
+              <h5 className="card-title">{rev.review_title}</h5>
+              <p className="card-text">{rev.review_desc}</p>
+              {/* <p className="card-text">Dev ID: {`${dev.id}`}</p> */}
+              <p className="card-text">Rating: {rev.rating}</p>
             </div>
           </div> 
         </div> ))}
