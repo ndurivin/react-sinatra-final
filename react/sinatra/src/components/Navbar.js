72477aae26634c8dd525597a756a890bcfdc166c
Vinn Nduri <ndurivo@gmail.com>
2022-09-15 07:22:12 +0300

    Centered the navlinks in navbar

--- react/sinatra/src/components/Navbar.js
+++ react/sinatra/src/components/Navbar.js
@@ -7,22 +7,22 @@ function Navbar() {
       
       <nav className="navbar fixed-top navbar-expand-lg bg-primary ">
         <div className="container">
-            <Link className="navbar-brand" style={{color: 'white'}} to="/">DevsLab</Link>
+            <Link className="navbar-brand" style={{color: 'white', fontSize: 28}} to="/">DevsLab</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
                 aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
             <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
+                <ul className="navbar-nav mx-auto" style={{fontSize: 16}}>
                 <li className="nav-item">
-                    <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
+                    <NavLink className="nav-link active" style={{color: 'white'}} aria-current="page" to="/">HOME</NavLink>
                 </li>
                 <li className="nav-item">
-                    <NavLink className="nav-link active"aria-current="page" to="/about">About</NavLink>
+                    <NavLink className="nav-link active" style={{color: 'white'}} aria-current="page" to="/about">ABOUT</NavLink>
                 </li>
                 <li className="nav-item">
-                    <NavLink className="nav-link active"  aria-current="page" to="/developers">Developers</NavLink>
+                    <NavLink className="nav-link active" style={{color: 'white'}} aria-current="page" to="/developers">DEVELOPERS</NavLink>
                 </li>
                 <li className="nav-item">
-                    <NavLink className="nav-link active" aria-current="page" to="/contact">Contact</NavLink>
+                    <NavLink className="nav-link active" style={{color: 'white'}} aria-current="page" to="/contact">CONTACT</NavLink>
                 </li>
                 </ul>
             </div>
--- react/sinatra/src/components/PostReview.js
+++ react/sinatra/src/components/PostReview.js
@@ -1,139 +1,143 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-// import { useLocation } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
-
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 function PostReview() {
+  const url = "http://localhost:9292/reviews";
 
-    const url = "http://localhost:9292/reviews"
-
-    const {state} = useLocation()
-    const {dev} = state
-    console.log(dev)
-
-   
-    const [inputs, setInputs] = useState({});
-    const [data, setData] = useState([])
-
-    const [review_title, setReview_Title] = useState('')
-    const [review_desc, setReview_Desc] = useState('')
-    // const [developer_id, setDeveloper_Id] = useState('')
-    const [rating, setRating] = useState('')
-
-// Get all data from api function using Axios
-
-useEffect(()=> {
-  axios.get(url)
-  .then(res => {
-    console.log(res.data)
-    setData(res.data)
-  })
-  .catch(err => console.log(err))
-
-}, [])
-
-// Post review to api function
-
-const postData = (e) => {
-  e.preventDefault();
-  axios.post(url, {
-    review_title,
-    review_desc, 
-    rating
-  }).then(res => console.log('Posting Review', res))
-    .catch(err => console.log(err))
-
-};
-
-//Update data function using PUT
+  const { state } = useLocation();
+  const { dev } = state;
+  console.log(dev);
 
 
+  const [data, setData] = useState([]);
 
+  const [review_title, setReview_Title] = useState("");
+  const [review_desc, setReview_Desc] = useState("");
+  // const [developer_id, setDeveloper_Id] = useState('')
+  const [rating, setRating] = useState("");
 
-const updateData = () => {
-//   fetch(`http://localhost:9292/reviews/`, {
+  // Get all data from api function using Axios
 
-//     method: "PUT",
-//     body: JSON.stringify({
-//       title: inputs.title,
-//       body: inputs.desc,
-//       rating: parseInt(inputs.rating)
-//     }),
-//     headers: {
-//       "Content-Type": "applicattion/json; characterset-UTF-8",
-//     },
-//   })
-//   .then((res)=> res.json())
-//   .then((json)=> console.log(json));
-};
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
+  // Post review to api function
 
-//Deleting data function 
+  const postData = (e) => {
+    e.preventDefault();
+    axios
+      .post(url, {
+        review_title,
+        review_desc,
+        rating,
+      })
+      .then((res) => console.log("Posting Review", res))
+      .catch((err) => console.log(err));
+  };
 
-const deleteData = (id, e) => {
-  e.preventDefault();
-  axios.delete(`http://localhost:9292/reviews/${id}`)
-  .then(res => console.log('Deleted!!', res))
-    .catch(err => console.log(err))
+  //Update data function using PUT
 
-};
+  const updateData = (id, e) => {
+    // e.preventDefault();
+    // axios.patch(`http://localhost:9292/reviews/${id}`)
+    // .then(res => console.log('Deleted!!', res))
+    //   .catch(err => console.log(err))
+  };
 
-
-
-//Handle change functionon the input data
-    // const handleChange=(event) => {
-    //   event.persist();
-    //   setInputs((input)=> ({
-    //     ...inputs,
-
-    //     [event.target.name]:event.target.value,
-
-    //   }))
-    // }
-
-    // const handleSubmit=(event)=>{
-    //   event.preventDefault();
-    //   postData();
-    //   // console.log(inputs)
-    // }
+  //Deleting data function
+  const deleteData = (id, e) => {
+    e.preventDefault();
+    axios
+      .delete(`http://localhost:9292/reviews/${id}`)
+      .then((res) => console.log("Deleted!!", res))
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div>
       <form>
-      <div className="mb-2">
-        <input type="text" value={review_title} onChange={(e) => setReview_Title(e.target.value)} name="review_title" placeholder='Review Title'></input>
+        <div className="mb-2">
+          <input
+            type="text"
+            value={review_title}
+            onChange={(e) => setReview_Title(e.target.value)}
+            name="review_title"
+            placeholder="Review Title"
+          ></input>
         </div>
         <div className="mb-2">
-        <input type="text" value={review_desc}  onChange={(e) => setReview_Desc(e.target.value)}  name="review_desc" placeholder='Review Description' row={2}></input>
+          <input
+            type="text"
+            value={review_desc}
+            onChange={(e) => setReview_Desc(e.target.value)}
+            name="review_desc"
+            placeholder="Review Description"
+            row={2}
+          ></input>
         </div>
         <div className="mb-2">
-        <input type="number" value={rating}  onChange={(e) => setRating(e.target.value)}  name="rating" placeholder='Rating' min={1} max={10}></input>
+          <input
+            type="number"
+            value={rating}
+            onChange={(e) => setRating(e.target.value)}
+            name="rating"
+            placeholder="Rating"
+            min={1}
+            max={10}
+          ></input>
         </div>
-        <button onClick={postData} type="button" className="btn btn-primary btn-sm" style={{width: 200}} >Add</button>
+        <button
+          onClick={postData}
+          type="button"
+          className="btn btn-primary btn-sm"
+          style={{ width: 200 }}
+        >
+          Add
+        </button>
       </form>
 
       <div className="p-5">
-        <h3 style={{color: "green"}}>Available Reviews</h3>
+        <h3 style={{ color: "green" }}>Available Reviews</h3>
         <div className="row-cols-md-4 py-2">
-      {data.map((rev)=> (  
-        <div key={rev.id} className="col mb-4">
-          <div className="card text-center h-100 shadow">
-            <div className="card-body ">
-              <h5 className="card-title">{rev.review_title}</h5>
-              <p className="card-text">{rev.review_desc}</p>
-              <p className="card-text">Rating: {rev.rating}</p>
+          {data.map((rev) => (
+            <div key={rev.id} className="col mb-4">
+              <div className="card text-center h-100 shadow">
+                <div className="card-body ">
+                  <h5 className="card-title">{rev.review_title}</h5>
+                  <p className="card-text">{rev.review_desc}</p>
+                  <p className="card-text">Rating: {rev.rating}</p>
+                </div>
+                <button
+                  onClick={updateData(rev.id)}
+                  type="button"
+                  className="btn btn-success btn-sm"
+                  style={{ margin: 5 }}
+                >
+                  Update
+                </button>
+                <button
+                  onClick={(e) => deleteData(rev.id, e)}
+                  type="button"
+                  className="btn btn-danger btn-sm"
+                  style={{ margin: 5 }}
+                >
+                  Delete
+                </button>
+              </div>
             </div>
-            <button onClick={updateData(data.id)} type="button" className="btn btn-success btn-sm" style={{margin: 5}}>Update</button>
-            <button onClick={(e) => deleteData(rev.id, e)} type="button" className="btn btn-danger btn-sm" style={{margin: 5}}>Delete</button>
-          </div> 
-        </div> ))}
+          ))}
+        </div>
       </div>
-      </div>
-
     </div>
-  )
+  );
 }
 
 export default PostReview;
